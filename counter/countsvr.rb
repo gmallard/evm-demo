9e0aa6706e6197e4d10ec29998995f3fc0cb6582
gmallard <allard.guy.m@gmail.com>
2010-01-10 13:38:35 -0500

    More code commenting in counter server example.

--- counter/countsvr.rb
+++ counter/countsvr.rb
@@ -6,32 +6,39 @@ require 'eventmachine'
 # Use the 'runtelnet.sh' script to connect to this server from multiple
 # terminal sessions for this example.
 #
-# Kill this sever by ^C.
+# Kill this sever by ^C or kill.
 #
 module LineCounter
   #
   MaxLinesPerConnection = 10
-  #
+
+  # EM::Connection.post_init override
   def post_init
-    puts "\nReceived a new connection"
+    puts "\n#{self.inspect} Received a new connection"
     @data_received = ""
     @line_count = 0
   end
-  #
+
+  # EM::Connection.receive_data override
   def receive_data data
     @data_received << data
     while @data_received.slice!( /^[^\n]*[\n]/m )
       @line_count += 1
-      send_data "received #{@line_count} lines so far\r\n"
+      data_out = "#{self.inspect} received #{@line_count} lines so far\r\n"
+      send_data data_out
+      puts data_out
+      # Force connection close after 10 lines received.
       @line_count == MaxLinesPerConnection and close_connection_after_writing
     end
   end
 end
+
 #
-# EM Runloop
+# EM Run/Event loop
 #
 EventMachine::run {
   host,port = "127.0.0.1", 8081
+  # No additional initialize parameters are used here.
   EventMachine::start_server host, port, LineCounter
   puts "Now accepting connections on address #{host}, port #{port}..."
   # Server heartbeat
